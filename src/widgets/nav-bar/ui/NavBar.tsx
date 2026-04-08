aec8f65829210b76dcbb253312d6f1a04eb9dd6b
Kirill Dubasov <cergocergo41@gmail.com>
2026-04-08 18:59:05 +0300

    feat(navbar): add NavBar widget root and barrel

---
+++ src/widgets/nav-bar/ui/NavBar.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import Image from 'next/image';
+
+import { useTranslations } from 'next-intl';
+import {
+  LuBookMarked,
+  LuBookOpen,
+  LuCoins,
+  LuHouse,
+  LuLayoutGrid,
+  LuListChecks,
+  LuSparkles,
+  LuUsers,
+} from 'react-icons/lu';
+
+import { Link } from '@/i18n/navigation';
+
+import { Divider } from './Divider';
+import styles from './NavBar.module.css';
+import { NavBarItem } from './NavBarItem';
+
+export function NavBar() {
+  const t = useTranslations('NavBar');
+
+  return (
+    <nav className={styles.nav}>
+      <Link href="/guides" className={styles.logoLink}>
+        <Image
+          src="/brand/academy-logo-full.svg"
+          alt="Open Academy"
+          width={175}
+          height={40}
+          className={styles.logoIcon}
+          priority
+        />
+      </Link>
+
+      <ul className={styles.list}>
+        <li>
+          <NavBarItem href="/home" icon={LuHouse} label={t('Главная')} />
+        </li>
+        <li>
+          <NavBarItem
+            href="/library"
+            icon={LuLayoutGrid}
+            label={t('Каталог')}
+          />
+        </li>
+        <li>
+          <NavBarItem
+            href="/my-courses"
+            icon={LuBookOpen}
+            label={t('Мои курсы')}
+          />
+        </li>
+      </ul>
+
+      <Divider />
+
+      <ul className={styles.list}>
+        <li>
+          <NavBarItem href="/tasks" icon={LuListChecks} label={t('Задания')} />
+        </li>
+        <li>
+          <NavBarItem href="/my-token" icon={LuCoins} label={t('Мой токен')} />
+        </li>
+        <li>
+          <NavBarItem
+            href="/referral-program"
+            icon={LuUsers}
+            label={t('Реферальная программа')}
+          />
+        </li>
+      </ul>
+
+      <Divider />
+
+      <ul className={styles.list}>
+        <li>
+          <NavBarItem
+            href="/guides"
+            icon={LuBookMarked}
+            label={t('Инструкции')}
+          />
+        </li>
+      </ul>
+
+      <Divider />
+
+      <ul className={styles.list}>
+        <li>
+          <NavBarItem
+            href="/rumi"
+            icon={LuSparkles}
+            label={t('Course Studio')}
+            disabled
+            badge={t('Скоро')}
+          />
+        </li>
+      </ul>
+    </nav>
+  );
+}
